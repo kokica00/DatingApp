60219c754b09fee6f6c506aa534eb91ae5923d61
unknown <koka_pozic@hotmail.com>
2019-01-30 15:39:04 +0100

    third party helperi

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,30 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 // najbolje je pro odmah sve oznaciti u app.modulu
 @Injectable({
-  providedIn: 'root'  // root predstavlja root modul... u ovom slucaju app.module.ts
+  providedIn: 'root' // root predstavlja root modul... u ovom slucaju app.module.ts
 })
 export class AuthService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
-baseUrl = 'http://localhost:5000/api/auth/';
+  constructor(private http: HttpClient) {}
 
-constructor(private http: HttpClient) { }
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log(this.decodedToken);
+        }
+      })
+    );
+  }
 
-login(model: any) {
-  return this.http.post(this.baseUrl + 'login', model)
-  .pipe(
-    map((response: any) => {
-      const user = response;
-      if (user) {
-        localStorage.setItem('token', user.token);
-      }
-    })
-  );
-}
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 
-register(model: any) {
-  return this.http.post(this.baseUrl + 'register', model);
-}
-
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 }
